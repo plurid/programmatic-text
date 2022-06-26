61effe1164c4a0e7afc9c19c605a66c81bc787a7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-06-26 22:03:01 +0300

    programmatic-text > dev: source

--- packages/programmatic-text/source/data/interfaces/index.ts
+++ packages/programmatic-text/source/data/interfaces/index.ts
@@ -1,10 +1,29 @@
 // #region module
 export interface ProgrammaticTextOptions {
-    timeout: number;
-    evaluationType: 'function' | 'variable';
     evaluationLanguage: 'javascript' | 'python';
+
+    /**
+     * Used when `evaluationLanguage` is `javascript`.
+     */
+    evaluationType: 'function' | 'variable';
+
+    /**
+     * Timeout for the code evaluation.
+     */
+    timeout: number;
+
+    /**
+     * Replace undefined variables with a certain string.
+     */
     replaceUndefined: string | undefined;
+
+    /**
+     * Catch errors from the evaluation of the code.
+     *
+     * Default `__error__`
+     */
     errorKey: string;
+
     logger: ((error: any) => void) | undefined;
 }
 // #endregion module
